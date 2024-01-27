95a14644db2cf3d4f2af7608540e24a6ce549b74
samv1974 <svasishat2003@gmail.com>
2024-01-27 16:47:10 +0530

    yeah boi

--- lib/pages/user_details_page.dart
+++ lib/pages/user_details_page.dart
@@ -1,6 +1,12 @@
+// import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 import 'package:projectssrk/components/my_button.dart';
 import 'package:projectssrk/components/my_textfield.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 class UserDetailsPage extends StatefulWidget {
   final Function()? onDetailsSubmitted;
@@ -17,7 +23,44 @@ class _UserDetailsPageState extends State<UserDetailsPage> {
   final TextEditingController parentsPhoneNumber = TextEditingController();
   final TextEditingController addressController = TextEditingController();
 
-  Future<void> save_user_details()
+  Future<void> getUserID(String name,int age,String parent_name,int parent_phone_number,String Address) async {
+    // Retrieve the currently signed-in user
+    User? user = FirebaseAuth.instance.currentUser;
+    String uid = user?.uid ?? ""; // Fetch the UID
+    print("User UID: $uid");
+    // print(name);
+    await save_user_details1(uid,name,age,parent_name,parent_phone_number,Address);
+  }
+
+  Future<void> save_user_details1(String uid,String name,int age,String parent_name,int parent_phone_number,String Address) async {
+    try {
+      final response = await http.post(
+        Uri.parse(
+            'http://127.0.0.1:5566/save_user_details'), // Replace with your Flask backend URL
+        headers: <String, String>{
+          'Content-Type': 'application/json; charset=UTF-8',
+        },
+        body: jsonEncode({
+          'uid': uid,
+          "child_name": name,
+          "child_age": age,
+          "parent_name": parent_name,
+          "parent_phone_number": parent_phone_number,
+          "address": Address,
+        }),
+      );
+
+      print('Response status: ${response.statusCode}');
+      print('Response body: ${response.body}');
+
+      if (response.statusCode != 200) {
+        throw Exception('Failed to register user on the server');
+      }
+    } catch (e) {
+      print('Error while saving user details: $e');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -70,7 +113,8 @@ class _UserDetailsPageState extends State<UserDetailsPage> {
             ElevatedButton(
               child: Text('Submit details'),
               onPressed: () {
-                widget.onDetailsSubmitted?.call();
+                getUserID(nameController.text,int.tryParse(ageController.text) ?? 0,parentsNameController.text,  int.tryParse(parentsPhoneNumber.text) ?? 0,addressController.text);
+
                 // You can add logic to handle the submission
               },
             ),
