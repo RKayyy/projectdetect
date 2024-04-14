bffd00b4a60fcbad9b5ee8eb32b8625dd7efee47
zedhog2002 <46309525+zedhog2002@users.noreply.github.com>
2024-04-15 03:06:05 +0530

    UI updated

---
+++ lib/pages/counting_quiz_history_page.dart
@@ -0,0 +1,94 @@
+import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_charts/charts.dart';
+
+class CountResultsPage extends StatelessWidget {
+  final Map<String, List<Map<String, dynamic>>> quizResults;
+
+  const CountResultsPage({Key? key, required this.quizResults})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // Filter out the results for quiz ID 2
+    final resultsForQuiz2 = quizResults['2'] ?? [];
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Quiz Results'),
+        backgroundColor: Color(0xFFF6F6F6), // Background color for app bar
+      ),
+      body: Container(
+        decoration: BoxDecoration(
+          color: Color(0xFFF6F6F6), // Background color for container
+          image: DecorationImage(
+            image: AssetImage('lib/images/background.png'), // Image asset
+            fit:
+                BoxFit.cover, // Adjusts the image to cover the entire container
+            colorFilter: ColorFilter.mode(
+              Colors.white.withOpacity(0.1), // 10% opacity (90% transparent)
+              BlendMode.dstATop, // Apply the opacity to the image
+            ),
+          ),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                'Quiz 2',
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            // Chart for quiz results
+            Expanded(
+              child: SfCartesianChart(
+                primaryXAxis: CategoryAxis(title: AxisTitle(text: 'Attempt')),
+                primaryYAxis:
+                    NumericAxis(title: AxisTitle(text: 'Average Score')),
+                series: [
+                  LineSeries<Map<String, dynamic>, int>(
+                    dataSource: resultsForQuiz2,
+                    xValueMapper: (result, index) => index + 1,
+                    yValueMapper: (result, _) => result['average_result'],
+                    name: 'Counting Quiz',
+                    markerSettings: MarkerSettings(
+                      isVisible: true,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            // ListView for individual attempts
+            Expanded(
+              child: ListView.builder(
+                shrinkWrap: true,
+                itemCount: resultsForQuiz2.length,
+                itemBuilder: (context, index) {
+                  final result = resultsForQuiz2[index];
+                  return Container(
+                    height: 60,
+                    width: 150,
+                    decoration: BoxDecoration(
+                      color: Colors.white, // Background color for the container
+                      borderRadius: BorderRadius.circular(
+                          15), // Radius for the curved corners
+                    ),
+                    child: ListTile(
+                      title: Text('Attempt ${index + 1}'),
+                      subtitle:
+                          Text('Average Result: ${result['average_result']}'),
+                    ),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/pages/result_history_page.dart
+++ lib/pages/result_history_page.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:fl_chart/fl_chart.dart';
 import 'package:syncfusion_flutter_charts/charts.dart';
 import 'package:syncfusion_flutter_charts/sparkcharts.dart';
+import 'package:projectssrk/pages/counting_quiz_history_page.dart';
 
 class ResultHistoryPage extends StatefulWidget {
   final String uid;
@@ -86,86 +87,154 @@ class _ResultHistoryPageState extends State<ResultHistoryPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Color(0xFFF6F6F6), // Background color
         title: Text('Result History'),
-      ),
-      body: Column(
-        children: [
-          // Separate chart for predictions
-          if (predictions.isNotEmpty)
-            SfCartesianChart(
-              primaryXAxis: CategoryAxis(title: AxisTitle(text: 'Prediction')),
-              primaryYAxis: NumericAxis(title: AxisTitle(text: 'Value')),
-              series: [
-                LineSeries<Map<String, dynamic>, String>(
-                  dataSource: predictions,
-                  xValueMapper: (prediction, _) =>
-                      'Prediction ${predictions.indexOf(prediction) + 1}',
-                  yValueMapper: (prediction, _) =>
-                      prediction['predicted_values'],
-                  name: 'Predictions',
-                  markerSettings: MarkerSettings(
-                    isVisible: true,
-                  ),
-                ),
-              ],
-            ),
-          // ListView for quiz results
-          Expanded(
-            child: ListView.builder(
-              itemCount: groupResultsByQuiz().length,
-              itemBuilder: (context, index) {
-                final entry = groupResultsByQuiz().entries.toList()[index];
-                final quizId = entry.key;
-                final quizResults = entry.value;
-
-                return Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Text(
-                        'Quiz $quizId',
-                        style: TextStyle(
-                            fontSize: 18, fontWeight: FontWeight.bold),
-                      ),
-                    ),
-                    // Chart for quiz results
-                    SfCartesianChart(
-                      primaryXAxis:
-                          CategoryAxis(title: AxisTitle(text: 'Attempt')),
-                      primaryYAxis:
-                          NumericAxis(title: AxisTitle(text: 'Average Score')),
-                      series: [
-                        LineSeries<Map<String, dynamic>, int>(
-                          dataSource: quizResults,
-                          xValueMapper: (result, _) =>
-                              quizResults.indexOf(result) + 1,
-                          yValueMapper: (result, _) => result['average_result'],
-                          name: 'Quiz $quizId',
-                          markerSettings: MarkerSettings(
-                            isVisible: true,
-                          ),
-                        ),
-                      ],
-                    ),
-                    // ListView for individual attempts
-                    ListView.builder(
-                      shrinkWrap: true,
-                      itemCount: quizResults.length,
-                      itemBuilder: (context, index) {
-                        return ListTile(
-                          title: Text('Attempt ${index + 1}'),
-                          subtitle: Text(
-                              'Average Result: ${quizResults[index]['average_result']}'),
-                        );
-                      },
-                    ),
-                  ],
-                );
-              },
+        flexibleSpace: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('lib/images/background.png'), // Image asset
+              fit:
+                  BoxFit.cover, // Adjusts the image to cover the entire app bar
+              colorFilter: ColorFilter.mode(
+                Colors.white.withOpacity(0.1), // 10% opacity (90% transparent)
+                BlendMode.dstATop, // Apply the opacity to the image
+              ),
             ),
           ),
-        ],
+        ),
+      ),
+      body: Container(
+        decoration: BoxDecoration(
+          color: Color(0xFFF6F6F6), // Background color
+          image: DecorationImage(
+            image: AssetImage('lib/images/background.png'), // Image asset
+            fit:
+                BoxFit.cover, // Adjusts the image to cover the entire container
+            colorFilter: ColorFilter.mode(
+              Colors.white.withOpacity(0.1), // 10% opacity (90% transparent)
+              BlendMode.dstATop, // Apply the opacity to the image
+            ),
+          ),
+        ),
+        child: Column(
+          children: [
+            SizedBox(height: 20),
+            Container(
+              alignment: Alignment.centerLeft,
+              padding: EdgeInsets.only(
+                  left: 50, top: 16, bottom: 16), // Padding from the left side
+              child: Text(
+                'Predictions',
+                style: TextStyle(
+                  fontSize: 32, // Font size
+                  fontFamily: 'Assistant', // Font family
+                ),
+              ),
+            ),
+            if (predictions.isNotEmpty)
+              Container(
+                width: 400, // Set the width to 310 pixels
+                decoration: BoxDecoration(
+                  color: Colors.white, // White background color
+                  borderRadius:
+                      BorderRadius.circular(14), // Rounded edges with radius 14
+                  boxShadow: [
+                    // Add a shadow for a more pronounced effect
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.3), // Shadow color
+                      spreadRadius: 2, // Spread radius
+                      blurRadius: 5, // Blur radius
+                      offset: Offset(0, 3), // Shadow offset
+                    ),
+                  ],
+                ),
+                child: SfCartesianChart(
+                  primaryXAxis:
+                      CategoryAxis(title: AxisTitle(text: 'Prediction')),
+                  primaryYAxis: NumericAxis(title: AxisTitle(text: 'Value')),
+                  series: [
+                    LineSeries<Map<String, dynamic>, String>(
+                      dataSource: predictions,
+                      xValueMapper: (prediction, _) =>
+                          'Prediction ${predictions.indexOf(prediction) + 1}',
+                      yValueMapper: (prediction, _) =>
+                          prediction['predicted_values'],
+                      markerSettings: MarkerSettings(
+                        isVisible: true,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+
+            SizedBox(height: 35),
+            Text(
+              'Your results are like stepping stones in \nthe playful garden of knowledge. Keep leaping, \nlittle explorer!',
+              textAlign: TextAlign.center, // Align text to the left
+              style: TextStyle(
+                fontSize: 14, // Font size 14
+                fontFamily: 'Assistant', // Font family "Assistant"
+              ),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () async {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) =>
+                        CountResultsPage(quizResults: groupResultsByQuiz()),
+                  ),
+                ); // Go back to the home page
+              },
+              style: ElevatedButton.styleFrom(
+                fixedSize: Size(270, 46),
+                backgroundColor: Color(0xFFEBC272), // Button color
+              ),
+              child: Text(
+                'Counting Quiz',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 24, // Text size 24
+                  fontWeight: FontWeight.bold, // Bold text
+                ),
+              ),
+            ),
+            SizedBox(height: 15), // Spacer for second button
+            ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                fixedSize: Size(270, 46),
+                backgroundColor: Color(0xFFEBC272), // Button color
+              ),
+              child: Text(
+                'Coloring Quiz',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 24, // Text size 24
+                  fontWeight: FontWeight.bold, // Bold text
+                ),
+              ),
+            ),
+            SizedBox(height: 15), // Spacer for third button
+            ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                fixedSize: Size(270, 46),
+                backgroundColor: Color(0xFFEBC272), // Button color
+              ),
+              child: Text(
+                'Calculating Quiz',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 24, // Text size 24
+                  fontWeight: FontWeight.bold, // Bold text
+                ),
+              ),
+            ),
+            // ListView for quiz results
+          ],
+        ),
       ),
     );
   }
