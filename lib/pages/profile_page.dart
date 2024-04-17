0d252229d48a95bdde8b656b62bf3025841893c8
zedhog2002 <46309525+zedhog2002@users.noreply.github.com>
2024-04-17 14:56:06 +0530

    profile_updates

--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -65,7 +65,7 @@ class _ProfilePageState extends State<ProfilePage> {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Color(0xFFF6F6F6), // Background color
-        title: Text('Result History'),
+        title: Text('Profile Page'),
         elevation: 0,
         flexibleSpace: Container(
           decoration: BoxDecoration(
@@ -95,20 +95,23 @@ class _ProfilePageState extends State<ProfilePage> {
         child: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
             children: [
               SizedBox(height: 16),
               Column(
                 children: [
                   Container(
+                    width: 270,
                     decoration: BoxDecoration(
                         borderRadius: BorderRadius.circular(15),
                         color: Color(0xFFEBC272)),
                     child: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
                         Text(
                           'User ID:\n',
                           style: TextStyle(fontSize: 12),
+                          textAlign: TextAlign.left,
                         ),
                         Text(
                           '$_uid',
@@ -118,85 +121,90 @@ class _ProfilePageState extends State<ProfilePage> {
                     ),
                   ),
                   Container(
+                    width: 270,
                     decoration: BoxDecoration(
                         borderRadius: BorderRadius.circular(15),
                         color: Color(0xFFEBC272)),
                     child: Column(
                       children: [
                         Text(
-                          'User ID:\n',
+                          'Child Name:\n',
                           style: TextStyle(fontSize: 12),
                         ),
                         Text(
-                          '$_uid',
+                          '${_userDetails['child_name'] ?? 'N/A'}',
                           style: TextStyle(fontSize: 20),
                         )
                       ],
                     ),
                   ),
                   Container(
+                    width: 270,
                     decoration: BoxDecoration(
                         borderRadius: BorderRadius.circular(15),
                         color: Color(0xFFEBC272)),
                     child: Column(
                       children: [
                         Text(
-                          'User ID:\n',
+                          'Child Age:\n',
                           style: TextStyle(fontSize: 12),
                         ),
                         Text(
-                          '$_uid',
+                          '${_userDetails['child_age'] ?? 'N/A'}',
                           style: TextStyle(fontSize: 20),
                         )
                       ],
                     ),
                   ),
                   Container(
+                    width: 270,
                     decoration: BoxDecoration(
                         borderRadius: BorderRadius.circular(15),
                         color: Color(0xFFEBC272)),
                     child: Column(
                       children: [
                         Text(
-                          'User ID:\n',
+                          'Parent Name:\n',
                           style: TextStyle(fontSize: 12),
                         ),
                         Text(
-                          '$_uid',
+                          '${_userDetails['parent_name'] ?? 'N/A'}',
                           style: TextStyle(fontSize: 20),
                         )
                       ],
                     ),
                   ),
                   Container(
+                    width: 270,
                     decoration: BoxDecoration(
                         borderRadius: BorderRadius.circular(15),
                         color: Color(0xFFEBC272)),
                     child: Column(
                       children: [
                         Text(
-                          'User ID:\n',
+                          'Parent Phone:\n',
                           style: TextStyle(fontSize: 12),
                         ),
                         Text(
-                          '$_uid',
+                          '${_userDetails['parent_phone_number'] ?? 'N/A'}',
                           style: TextStyle(fontSize: 20),
                         )
                       ],
                     ),
                   ),
                   Container(
+                    width: 270,
                     decoration: BoxDecoration(
                         borderRadius: BorderRadius.circular(15),
                         color: Color(0xFFEBC272)),
                     child: Column(
                       children: [
                         Text(
-                          'User ID:\n',
+                          'Address:\n',
                           style: TextStyle(fontSize: 12),
                         ),
                         Text(
-                          '$_uid',
+                          '${_userDetails['address'] ?? 'N/A'}',
                           style: TextStyle(fontSize: 20),
                         )
                       ],
